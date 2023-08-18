52862cb659f2989e18dc128f6b3f486e0d0e28ec
Student <student@gnu.ac.in>
2023-08-18 12:27:06 +0530

    18/8/23

--- app/src/main/java/com/example/mad_practical_5_22172012006/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_5_22172012006/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.provider.CallLog
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
 import java.time.Instant
@@ -16,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         val editTextBrowser:EditText = findViewById(R.id.editTextText)
         browserButton.setOnClickListener {
             openurl(editTextBrowser.text.toString())
+
         }
     }
 
@@ -29,6 +33,18 @@ class MainActivity : AppCompatActivity() {
     }
     fun calllog()
     {
-
+        Intent(Intent.ACTION_VIEW).setType(CallLog.Calls.CONTENT_TYPE).also { startActivity(it) }
+    }
+    fun gallery()
+    {
+        Intent(Intent.ACTION_VIEW).setType("image/*").also { startActivity(it)}
+    }
+    fun camera()
+    {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { startActivity(it) }
+    }
+    fun alarm()
+    {
+    Intent(AlarmClock.ACTION_SHOW_ALARMS).also { startActivity(it) }
     }
 }
